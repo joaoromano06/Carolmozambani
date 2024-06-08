5938a0826deaa6f68a0c5df9228dbfab45d8f014
joaoromano06 <joaoromano06@gmail.com>
2024-06-08 11:10:57 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
+const menuToggle = document.getElementById('menuToggle');
+const mainMenu = document.getElementById('mainMenu');
+
+menuToggle.addEventListener('click', () => {
+    mainMenu.classList.toggle('show'); 
+});
+
+const links = document.querySelectorAll('nav ul li a'); 
+
+links.forEach(link => {
+    link.addEventListener('click', (event) => {
+        event.preventDefault(); 
+        const targetId = link.getAttribute('href');
+        const targetElement = document.querySelector(targetId);
+
+        if (targetElement) {
+            window.scrollTo({
+                top: targetElement.offsetTop - 60, // Ajuste para o header
+                behavior: 'smooth' // Rolagem suave
+            });
+        }
     });
 });
